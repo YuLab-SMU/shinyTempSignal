4305acc5fa2d0f0b52cc23fd8ef505c25833e044
xuanan <1063785553@qq.com>
2021-08-27 10:34:30 +0800

    修改数据

--- sTSpkg/dev/02_dev.R
+++ sTSpkg/dev/02_dev.R
@@ -16,6 +16,14 @@
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
 usethis::use_package( "thinkr" )
+usethis::use_package( "Cairo" )
+usethis::use_package( "ape" )
+usethis::use_package( "ggplot2" )
+usethis::use_package( "ggpubr" )
+usethis::use_package( "ggtree" )
+usethis::use_package( "shinydashboard" )
+usethis::use_package( "shinyjs" )
+usethis::use_package( "stringr" )
 
 ## Add modules ----
 ## Create a module infrastructure in R/
